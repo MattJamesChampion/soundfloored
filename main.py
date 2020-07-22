4058d2086f34aa7be8e0a4d5038619a208213fd4
Matthew Champion <mattjameschampion@gmail.com>
2020-07-22 18:57:43 +0100

    Add a settings file and a a corresponding class as well as the logic to load/initialise them

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import configparser
+
+class Settings:
+    def __init__(self, config_parser):
+        try:
+            self.root_audio_directory = config_parser["DEFAULT"]["RootAudioDirectory"]
+        except KeyError:
+            print("Could not find the root audio directory")
+            raise
+
+def get_settings(path):
+    config_parser = configparser.ConfigParser()
+    config_parser.read(path)
+
+    return Settings(config_parser)
+
 def main():
-    pass
+    settings = get_settings("settings.ini")
 
 if __name__ == "__main__":
     main()
