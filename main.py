3e5f0b47544c43de562fc21cbd084bef463535c0
Matthew Champion <mattjameschampion@gmail.com>
2020-07-22 19:20:32 +0100

    Remember to add load_banks otherwise the banks... won't get loaded

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
     settings = get_settings("settings.ini")
 
     music_logic = MusicLogic(settings.root_audio_directory)
+    music_logic.load_banks()
 
 if __name__ == "__main__":
     main()
