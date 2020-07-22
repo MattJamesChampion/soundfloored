28240cd6075dffdfd208debdd24df8fe9edccdc9
Matthew Champion <mattjameschampion@gmail.com>
2020-07-22 20:59:22 +0100

    Add logic to keyboard_interface

--- soundfloored/interfaces/keyboard_interface.py
+++ soundfloored/interfaces/keyboard_interface.py
@@ -1,3 +1,27 @@
+import pygame
+
 class KeyboardInterface:
-    def __init__(self):
-        pass
+    def __init__(self, music_logic):
+        self.music_logic = music_logic
+
+    def start(self):
+        pygame.display.init()
+        screen = pygame.display.set_mode((1,1))
+        current_bank = self.music_logic.get_current_bank()
+
+        available_clip_keys = [
+            pygame.K_a,
+            pygame.K_s,
+            pygame.K_d,
+            pygame.K_f
+        ]
+
+        while True:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.KEYDOWN:
+                    try:
+                        clip_position = available_clip_keys.index(event.key)
+                        current_bank.play_clip(clip_position)
+                    except:
+                        pass
