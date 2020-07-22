918bde1a322a0d3fbf85df73199258acb3f0818e
Matthew Champion <mattjameschampion@gmail.com>
2020-07-22 23:25:12 +0100

    Add logic to KeyboardInterface to modify the current bank

--- soundfloored/interfaces/keyboard_interface.py
+++ soundfloored/interfaces/keyboard_interface.py
@@ -20,6 +20,10 @@ class KeyboardInterface:
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        current_bank = self.music_logic.decrement_bank()
+                    if event.key == pygame.K_r:
+                        current_bank = self.music_logic.increment_bank()
                     try:
                         clip_position = available_clip_keys.index(event.key)
                         current_bank.play_clip(clip_position)
