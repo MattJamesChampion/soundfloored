900f41b4dca97a47667d2a7d353d263b3d5160b6
Matthew Champion <mattjameschampion@gmail.com>
2020-07-25 23:25:24 +0100

    Move to a more functional style of dealing with control actions on KeyboardInterface

--- soundfloored/interfaces/keyboard_interface.py
+++ soundfloored/interfaces/keyboard_interface.py
@@ -8,6 +8,14 @@ class KeyboardInterface:
         pygame.display.init()
         screen = pygame.display.set_mode((1,1))
 
+        available_control_keys = {
+            pygame.K_1: self.music_logic.move_previous_repeat_style,
+            pygame.K_4: self.music_logic.move_next_repeat_style,
+            pygame.K_q: self.music_logic.decrement_bank,
+            pygame.K_r: self.music_logic.increment_bank,
+            pygame.K_SPACE: self.music_logic.stop_all_clips,
+        }
+
         available_clip_keys = [
             pygame.K_a,
             pygame.K_s,
@@ -19,16 +27,11 @@ class KeyboardInterface:
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_1:
-                        self.music_logic.move_previous_repeat_style()
-                    if event.key == pygame.K_4:
-                        self.music_logic.move_next_repeat_style()
-                    if event.key == pygame.K_q:
-                        self.music_logic.decrement_bank()
-                    if event.key == pygame.K_r:
-                        self.music_logic.increment_bank()
-                    if event.key == pygame.K_SPACE:
-                        self.music_logic.stop_all_clips()
+                    try:
+                        available_control_keys[event.key]()
+                    except:
+                        pass
+
                     try:
                         clip_position = available_clip_keys.index(event.key)
                         self.music_logic.play_clip(clip_position)
