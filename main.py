6ffc5ae20f7a46dfd3f011ba96261c447fa440dc
Matthew Champion <mattjameschampion@gmail.com>
2020-07-25 21:50:26 +0100

    Add detailed debug and error logging

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import configparser
+import logging
+import sys
 from soundfloored.music_logic import MusicLogic
 from soundfloored.interfaces.keyboard_interface import KeyboardInterface
 from soundfloored.interfaces.gui_interface import GuiInterface
 
 class Settings:
     def __init__(self, config_parser):
+        self._logger = logging.getLogger(__name__)
         try:
             self.root_audio_directory = config_parser["DEFAULT"]["RootAudioDirectory"]
         except KeyError:
-            print("Could not find the root audio directory setting")
+            self._logger.error("Could not find the root audio directory setting")
             raise
 
         try:
             self.interface = config_parser["DEFAULT"]["Interface"]
         except KeyError:
-            print("Could not find the interface setting")
+            self._logger.error("Could not find the interface setting")
             raise
 
 def get_settings(path):
@@ -26,13 +29,16 @@ def get_settings(path):
 def main():
     settings = get_settings("settings.ini")
 
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     music_logic = MusicLogic(settings.root_audio_directory)
 
     interface = None
 
     if settings.interface == "keyboard":
+        logging.debug("Creating instance of KeyboardInterface")
         interface = KeyboardInterface(music_logic)
     elif settings.interface == "gui":
+        logging.debug("Creating instance of GuiInterface")
         interface = GuiInterface(music_logic)
 
     interface.start()
--- soundfloored/music_logic.py
+++ soundfloored/music_logic.py
@@ -1,6 +1,7 @@
 import os
 import pygame
 from enum import Enum
+import logging
 
 class RepeatStyle(Enum):
     #Values must be sequential starting from 0
@@ -14,6 +15,7 @@ class Bank:
 
 class MusicLogic:
     def __init__(self, root_audio_directory):
+        self._logger = logging.getLogger(__name__)
         self.root_audio_directory = root_audio_directory
         self.repeat_style = RepeatStyle.RESTART
         self.banks = []
@@ -34,6 +36,7 @@ class MusicLogic:
         if repeat_style == None:
             repeat_style = self.repeat_style
 
+        self._logger.debug(f"play_clip executing with position {position}, bank {bank.name} and repeat_style {repeat_style}")
         try:
             #TODO: Increase number of channels if position is greater than the default of 10
         
@@ -47,7 +50,7 @@ class MusicLogic:
                 else:
                     pygame.mixer.Channel(position).play(clip)
         except IndexError:
-            pass
+            self._logger.debug(f"play_clip referenced an invalid index (requested position {position} of a bank with only {len(bank.clips)} elements)")
 
 
     def increment_bank(self):
@@ -76,6 +79,7 @@ class MusicLogic:
     @current_bank_position.setter
     def current_bank_position(self, value):
         self._current_bank = value
+        self._logger.debug(f"Bank position set to {self._current_bank}")
 
     def move_next_repeat_style(self):
         maximum_repeat_style = len(RepeatStyle) - 1
@@ -83,6 +87,8 @@ class MusicLogic:
             self.repeat_style = RepeatStyle(0)
         else:
             self.repeat_style = RepeatStyle(self.repeat_style.value + 1)
+
+        self._logger.debug(f"Repeat style set to {self.repeat_style.name}")
 
         return self.repeat_style
     
@@ -93,17 +99,22 @@ class MusicLogic:
         else:
             self.repeat_style = RepeatStyle(self.repeat_style.value - 1)
 
+        self._logger.debug(f"Repeat style set to {self.repeat_style.name}")
+
         return self.repeat_style
 
     def load_banks(self):
         pygame.mixer.init()
         
         banks = []
+        self._logger.debug(f"Loading banks (with root directory {self.root_audio_directory})")
         for bank in os.listdir(self.root_audio_directory):
             bank_path = os.path.join(self.root_audio_directory, bank)
+            self._logger.debug(f"Loading bank {bank_path}")
             clips = []
             for clip in os.listdir(bank_path):
                 clip_path = os.path.join(bank_path, clip)
+                self._logger.debug(f"Loading clip {clip_path}")
                 clip_sound = pygame.mixer.Sound(clip_path)
                 clips.append(clip_sound)
 
