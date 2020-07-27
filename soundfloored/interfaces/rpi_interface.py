b0666d779612c5e4239001fc065eee59c0a6098c
Matthew Champion <mattjameschampion@gmail.com>
2020-07-27 22:17:44 +0100

    Fix an error with RpiInterface caused by add_event_detect passing a channel value along to the callback function

--- soundfloored/interfaces/rpi_interface.py
+++ soundfloored/interfaces/rpi_interface.py
@@ -9,14 +9,18 @@ class RpiInterface:
     def start(self):
         pygame.display.init()
         screen = pygame.display.set_mode((1,1))
-        
+
         GPIO.setmode(GPIO.BCM)
 
         audio_clip_button_pins = [19, 13, 6, 5]
 
+        # Used to discard the channel value that is automatically provided by
+        # add_event_detect to the callback function
+        callback_wrapper = lambda index, channel: self.music_logic.play_clip(index)
+
         for index, audio_clip_button_pin in enumerate(audio_clip_button_pins):
             GPIO.setup(audio_clip_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(audio_clip_button_pin, GPIO.FALLING, callback=partial(self.music_logic.play_clip, index))    
+            GPIO.add_event_detect(audio_clip_button_pin, GPIO.FALLING, callback=partial(callback_wrapper, index))
         
         while True:
             pass
